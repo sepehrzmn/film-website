20be9cda097be16b11537fa007100fcd89ce29f3
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-03 18:44:28 +0430

    added Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+
 import Navbar from "@components/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "@routes/Home/Home";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
 		</div>
 	);
 }
