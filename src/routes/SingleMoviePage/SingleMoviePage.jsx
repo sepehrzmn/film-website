9447fc7db93328b4618eaf1a93fb0004a6280de1
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-07 20:06:01 +0430

    updated single page movie

--- src/routes/SingleMoviePage/SingleMoviePage.jsx
+++ src/routes/SingleMoviePage/SingleMoviePage.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 
-import { InitialTransition } from "@components";
+import { useParams } from "react-router-dom";
+
+import { InitialTransition, IntoSingePageMovie } from "@components";
+import { motion } from "framer-motion";
 
 function SingleMoviePage() {
+	const { movieId } = useParams();
+	const animePage = {
+		initial: {
+			y: -100,
+			opacity: 0,
+		},
+		animate: {
+			y: 0,
+			opacity: 1,
+			transition: {
+				duration: 0.9,
+				delay: 0.5,
+				ease: [0.6, -0.05, 0.01, 0.99],
+			},
+		},
+	};
+	if (!movieId) {
+		<div className="w-full h-full flex justify-center items-center ">
+			movie not Found!
+		</div>;
+	}
 	return (
-		<>
+		<motion.div
+			exit={{
+				opacity: 0,
+			}}
+		>
 			<InitialTransition />
-		</>
+			<motion.div initial="initial" animate="animate" variants={animePage}>
+				<IntoSingePageMovie movieId={Number(movieId)} />
+			</motion.div>
+		</motion.div>
 	);
 }
 
