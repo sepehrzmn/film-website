c41f6a25ed8b0d220c0b274e69fdc5f00ca15ed2
sepehrzmn <95043491+sepehrzmn@users.noreply.github.com>
2022-06-07 20:08:47 +0430

    updated AnimatedRoutes component

--- src/components/AnimatedRoutes/AnimatedRoutes.js
+++ src/components/AnimatedRoutes/AnimatedRoutes.js
@@ -3,7 +3,7 @@ import React from "react";
 import { AnimatePresence } from "framer-motion";
 
 function AnimatedRoutes({ children }) {
-	return <AnimatePresence>{children}</AnimatePresence>;
+	return <AnimatePresence exitBeforeEnter>{children}</AnimatePresence>;
 }
 
 export default AnimatedRoutes;
